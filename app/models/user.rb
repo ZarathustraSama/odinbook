d4c105d41d0b444015d2894fb75e7c4b5b366796
ZarathustraSama <mr.gabriel@live.it>
2024-08-26 10:52:49 +0200

    Added comment model with user/post relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :likes
+  has_many :comments
 
   has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
   has_many :followees, through: :followed_users
