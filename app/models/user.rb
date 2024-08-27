96ebc8adf60c93013f98eeea330adbb4cf4569e2
ZarathustraSama <mr.gabriel@live.it>
2024-08-27 09:42:36 +0200

    Added list of omniauth providers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable
+         :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2, :twitter2]
 
   has_many :posts
   has_many :likes
