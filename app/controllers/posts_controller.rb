779a3de297a66fa9bb6791af73f9c4a7eef80d8d
ZarathustraSama <mr.gabriel@live.it>
2024-09-13 10:24:21 +0200

    Defined main restful action for view/create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    if @user.posts.create(post_params)
+      redirect_to @user
+    else
+      flash[:alert] = 'Something went wrong'
+      render root_path, status: :unprocessable_entity
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :image_url)
   end
 end
