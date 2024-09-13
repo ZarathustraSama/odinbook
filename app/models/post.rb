050832e259d2eb660c34a43da8be3b2e68b515da
ZarathustraSama <mr.gabriel@live.it>
2024-09-13 10:44:55 +0200

    Reworked Post model/schema to simplify polymorphism

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :postable, polymorphic: true
   belongs_to :user
 
   has_many :likes
   has_many :comments
+
+  validates_presence_of(:body)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_06_075859) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_10_094809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,12 +30,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_075859) do
     t.string "status"
   end
 
-  create_table "images", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -44,18 +38,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_06_075859) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "postable_type", null: false
-    t.bigint "postable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.index ["postable_type", "postable_id"], name: "index_posts_on_postable"
-  end
-
-  create_table "texts", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "image_url"
+    t.string "body"
   end
 
   create_table "users", force: :cascade do |t|
