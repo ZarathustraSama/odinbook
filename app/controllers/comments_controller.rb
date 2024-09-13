d3709d638a4bffa18cea4c1b84b3724d3857f657
ZarathustraSama <mr.gabriel@live.it>
2024-09-13 12:39:58 +0200

    Can now view/post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,20 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    post = Post.find(comment_params[:id])
+    comment = post.comments.build(comment_params)
+    comment.user_id = current_user.id
+    if comment.save!
+      redirect_back_or_to(root_path)
+    else
+      flash[:alert] = 'Something went wrong'
+      redirect_to root_path
+    end
   end
 
   private
 
   def comment_params
+    params.require(:comment).permit(:id, :content)
   end
 end
