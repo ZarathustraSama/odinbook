191986bc36952c1b673d2f4bc771f2cc1ac612e8
ZarathustraSama <mr.gabriel@live.it>
2024-09-13 10:47:13 +0200

    Added basic user actions for viewing users/profile, including post owned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: params[:id]).reverse_order
   end
 end
