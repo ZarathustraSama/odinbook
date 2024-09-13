c1f85e428cacf163d683f283f5a5f8d409df78cc
ZarathustraSama <mr.gabriel@live.it>
2024-09-13 11:58:46 +0200

    Can now like/dislike posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,34 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
+
   def create
+    like = Like.where("user_id = ? AND post_id = ?", current_user.id, like_params[:id])
+    if like.any?
+      flash[:alert] = 'You already liked this post!'
+    else
+      like = Like.new
+      like.user_id = current_user.id
+      like.post_id = like_params[:id]
+      like.save!
+    end
+
+    redirect_back_or_to(root_path)
+  end
+
+  def destroy
+    like = Like.where("user_id = ? AND post_id = ?", current_user.id, like_params[:id]).first
+    if like.destroy
+      redirect_back_or_to(root_path)
+    else
+      flash[:alert] = 'Something went wrong'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def like_params
+    params.require(:post).permit(:id)
   end
 end
