6343fd5a8317cab61ffae06623d2680ad7f1f51f
ZarathustraSama <mr.gabriel@live.it>
2024-09-16 10:18:30 +0200

    Fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_many :followed_users, foreign_key: :follower_id, class_name: "Follow", depentent: :destroy
+  has_many :followed_users, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
   has_many :followees, through: :followed_users
 
   has_many :following_users, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
